572c13273417b1b20d427d32b42a8f4fb84ff7e1
Tim Miller <tim_miller@users.sourceforge.net>
2002-08-23 06:40:53 +0000

    Added support to DroppableList for dropping directories.  Their contents are now recursively added.

--- com/gallery/GalleryRemote/DroppableList.java
+++ com/gallery/GalleryRemote/DroppableList.java
@@ -26,6 +26,11 @@ import java.io.*;
 import java.awt.datatransfer.*;
 import java.awt.dnd.*;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Iterator;
+import java.util.ArrayList;
+
 /**
  *  Drag and drop handler
  *
@@ -35,6 +40,8 @@ import java.awt.dnd.*;
 public class DroppableList extends JList
 		 implements DropTargetListener
 {
+	private static final String MODULE = "Droplist";
+	
 	DropTarget dropTarget = new DropTarget( this, this );
 	MainFrame mDaddy = null;
 
@@ -103,12 +110,26 @@ public class DroppableList extends JList
 						DnDConstants.ACTION_COPY_OR_MOVE );
 				java.util.List fileList = (java.util.List)
 						tr.getTransferData( DataFlavor.javaFileListFlavor );
-
-				mDaddy.addPictures( (File[]) fileList.toArray( new File[0] ) );
-
+				
+				/* recursively add contents of directories */
+				java.util.List allFilesList = null;
+				try {
+					allFilesList = expandDirectories( fileList );
+				} catch ( IOException ioe ) {
+					Log.log( Log.ERROR, MODULE, "i/o exception listing dirs in a drop");
+					Log.logStack( Log.ERROR, MODULE );
+					JOptionPane.showMessageDialog(
+						null,
+						"It was not possible to accept the images due to an error.",
+						"Error Accepting Dragged Images",
+						JOptionPane.ERROR_MESSAGE);
+				}
+				mDaddy.addPictures( (File[]) allFilesList.toArray( new File[0] ) );
+				
 				dropTargetDropEvent.getDropTargetContext().dropComplete( true );
+				
 			} else {
-				System.err.println( "Rejected" );
+				Log.log(Log.TRACE, MODULE, "rejecting drop: javaFileListFlavor is not supported for this data");
 				dropTargetDropEvent.rejectDrop();
 			}
 		} catch ( IOException io ) {
@@ -118,6 +139,69 @@ public class DroppableList extends JList
 			ufe.printStackTrace();
 			dropTargetDropEvent.rejectDrop();
 		}
+	}
+	
+	/**
+	 *  Given a list of files and directories, the list is expanded
+	 *	recursively and returned with only files.
+	 *
+	 *	@param filesAndFolders	Files and/or directories.
+	 */
+	java.util.List expandDirectories( java.util.List filesAndFolders ) throws IOException {
+		
+		java.util.ArrayList allFilesList = new ArrayList();
+		
+		Iterator iter = filesAndFolders.iterator();
+		while ( iter.hasNext() ){
+			File f = (File)iter.next();
+			if ( f.isDirectory() ) {
+				allFilesList.addAll( listFilesRecursive( f ) );
+			} else {
+				allFilesList.add( f );
+			}
+		}
+		
+		return allFilesList;
+	}
+	
+	/**
+	 *  Given a directory, this method returns a recursive list of all the
+	 *	file contents of that directory.  It does not include the directories
+	 *	in the resulting List.
+	 *	
+	 *	@param dir A directory to list recursively.  If the file is not a
+	 *		directory, an empty list is returned.
+	 */
+	static java.util.List listFilesRecursive( File dir ) throws IOException {
+		ArrayList ret = new ArrayList();
+		
+		/* File.listFiles: stupid call returns null if there's an 
+		   i/o exception *or* if the file is not a directory, making a mess.
+		   http://java.sun.com/j2se/1.4/docs/api/java/io/File.html#listFiles() */
+		File [] fileArray = dir.listFiles();
+		if ( fileArray == null ) {
+			if ( dir.isDirectory() ) {
+				/* convert to exception */
+				throw new IOException( "i/o exception listing directory: " + dir.getPath() );
+			} else {
+				/* this method should only be called on a directory */
+				Log.log(Log.CRITICAL, MODULE, "assertion failed: listFilesRecursive called on a non-dir file");
+				return ret;
+			}
+		}
+		java.util.List files = Arrays.asList( fileArray );
+		
+		Iterator iter = files.iterator();
+		while( iter.hasNext() ) {
+			File f = (File)iter.next();
+			if ( f.isDirectory() ) {
+				ret.addAll( listFilesRecursive( f ) );
+			} else {
+				ret.add( f );
+			}	
+		}
+		
+		return ret;
 	}
 }
 
