b497ed6b183f4bb54cf021a99453609984c53291
Pierre-Luc Paour <paour@users.sourceforge.net>
2002-09-01 08:39:46 +0000

    New script to create a daily build and stage it on the web server so Jesse Mullan can grab and host it.

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+./make.sh cvsbuild;
+
+version=`grep version= defaults.properties | awk -F= '{print $2}'`;
+echo $version;
+
+rm /var/www/html/gr-staging/*
+cp gallery_remote.zip /var/www/html/gr-staging/gallery_remote_$version.zip
