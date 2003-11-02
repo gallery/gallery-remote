31cc2c5ff00e710d628c1aa220a568fa238c0707
Pierre-Luc Paour <paour@users.sourceforge.net>
2003-11-02 02:51:55 +0000

    Updated path (this is not very portable...)

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ cvs -q update -d;
 version=`grep version= defaults.properties | awk -F= '{print $2}'`;
 echo $version;
 
-rm /var/www/html/gr-staging/*
-cp gallery_remote.zip /var/www/html/gr-staging/gallery_remote_$version.zip
+rm /home/httpd/htdocs/gr-staging/*
+cp gallery_remote.zip /home/httpd/htdocs/gr-staging/gallery_remote_$version.zip
 
 lynx -dump -head http://jpmullan.com/galleryupdates/remote/gallery_remote_$version.zip | grep "HTTP/1.1 404" && scp gallery_remote.zip paour@jpmullan.com:remote/gallery_remote_$version.zip && . ant post
