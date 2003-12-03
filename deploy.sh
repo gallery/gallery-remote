f0c17cacbad25c239eb039ea2e807a73aba5c85d
Pierre-Luc Paour <paour@users.sourceforge.net>
2003-12-03 16:31:39 +0000

    Only remving ZIP files from staging.

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ cvs -q update -d;
 version=`grep version= defaults.properties | awk -F= '{print $2}'`;
 echo $version;
 
-rm /home/httpd/htdocs/gr-staging/*
+rm /home/httpd/htdocs/gr-staging/*.zip
 cp gallery_remote.zip /home/httpd/htdocs/gr-staging/gallery_remote_$version.zip
 
 lynx -dump -head http://jpmullan.com/galleryupdates/remote/gallery_remote_$version.zip | grep "HTTP/1.1 404" && scp gallery_remote.zip paour@jpmullan.com:remote/gallery_remote_$version.zip && . ant post
