195ded737ce161fd5c265d6f77ca9df2570b09c2
Pierre-Luc Paour <paour@users.sourceforge.net>
2003-12-31 07:27:31 +0000

    Tweaked nightly build so that only one compilation pass is needed

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,15 @@
 cvs -q update -d;
 
-. ant zip;
+. ant nightly;
 
 version=`grep version= defaults.properties | awk -F= '{print $2}'`;
 echo $version;
 
-cp gallery_remote.zip /home/httpd/htdocs/gr-staging/gallery_remote_${version}.zip
-
-. ant installer_macosx
-
-cp GalleryRemote.${version}.MacOSX.NoVM.tgz /home/httpd/htdocs/gr-staging
+cp gallery_remote.zip /home/httpd/htdocs/gr-staging/gallery_remote_${version}.zip;
+cp GalleryRemote.${version}.MacOSX.NoVM.tgz /home/httpd/htdocs/gr-staging;
 
 lynx -dump -head http://jpmullan.com/galleryupdates/remote/gallery_remote_${version}.zip \
 	| grep "HTTP/1.1 404" && \
 	scp gallery_remote.zip paour@jpmullan.com:remote/gallery_remote_${version}.zip && \
 	scp GalleryRemote.${version}.MacOSX.NoVM.tgz paour@jpmullan.com:remote/ && \
-	. ant post
+	. ant post;
