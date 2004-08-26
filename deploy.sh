f9f87849a8554ef005b8ec9ab150f2fbd169ebc2
Pierre-Luc Paour <paour@users.sourceforge.net>
2004-08-26 16:54:52 +0000

    Updated bightly build system

--- deploy.sh
+++ deploy.sh
@@ -5,17 +5,13 @@ cvs -q update -d;
 version=`grep version= defaults.properties | awk -F= '{print $2}'`;
 echo -e "\n\nVersion: $version\n\n";
 
-#cp gallery_remote.zip /home/httpd/htdocs/gr-staging/gallery_remote_${version}.zip;
-#cp gallery_remote_applets.zip /home/httpd/htdocs/gr-staging/gallery_remote_applets_${version}.zip;
-#cp GalleryRemote.MacOSX.NoVM.tgz /home/httpd/htdocs/gr-staging/GalleryRemote.${version}.MacOSX.NoVM.tgz;
-
 echo -e "Checking nightly site...\n\n";
 if (lynx -dump -head http://jpmullan.com/galleryupdates/remote/gallery_remote_${version}.zip \
 	| grep "HTTP/1.1 404") then
 	echo -e "\n\nWe have a new version.";
 
 	echo -e "\n\nBuilding...\n\n";
-	. ant nightly;
+	ant nightly;
 
 	echo -e "\n\nUploading to the nightly site...\n\n";
 	scp gallery_remote.zip paour@jpmullan.com:remote/gallery_remote_${version}.zip;
@@ -26,7 +22,7 @@ if (lynx -dump -head http://jpmullan.com/galleryupdates/remote/gallery_remote_${
 	ssh paour@jpmullan.com "cp -pf remote/GalleryRemote.${version}.MacOSX.NoVM.tgz remote/current_GalleryRemote.MacOSX.NoVM.tgz";
 	
 	echo -e "\n\nPosting new version info on version check site...\n\n";
-	. ant post;
+	ant post;
 	
 	if ( [ -f deploy_local.sh ] ) then
 		echo -e "\n\nRunning local script...\n\n";
