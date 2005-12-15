31455c5d095c7e3ff6e9fa2b5c4356f7813f0e24
Pierre-Luc Paour <paour@users.sourceforge.net>
2005-12-15 08:00:37 +0000

    2005-12-15  Pierre-Luc Paour <gallery@paour.com> (1.5.1-b6)
    	* Fixed a bug introduced in b5 which broke the slideshow.

--- com/gallery/GalleryRemote/prefs/GalleryProperties.java
+++ com/gallery/GalleryRemote/prefs/GalleryProperties.java
@@ -89,6 +89,7 @@ public class GalleryProperties extends Properties implements PreferenceNames {
 		}
 	}
 
+	/* Adapted from Properties to support UTF-8 */
 	public void load(String data) throws IOException {
 		BufferedReader in = new BufferedReader(new StringReader(data));
 		while (true) {
@@ -144,10 +145,56 @@ public class GalleryProperties extends Properties implements PreferenceNames {
 					String value = (separatorIndex < len) ? line.substring(valueIndex, len) : "";
 
 					// Convert then store key and value
-					put(key, value);
+					put(loadConvert(key), loadConvert(value));
 				}
 			}
 		}
+	}
+
+	public String loadConvert (String theString) {
+	    char aChar;
+	    int len = theString.length();
+	    StringBuffer outBuffer = new StringBuffer(len);
+
+	    for(int x=0; x<len; ) {
+	        aChar = theString.charAt(x++);
+	        if (aChar == '\\') {
+	            aChar = theString.charAt(x++);
+	            if(aChar == 'u') {
+	                // Read the xxxx
+	                int value=0;
+			for (int i=0; i<4; i++) {
+			    aChar = theString.charAt(x++);
+			    switch (aChar) {
+			      case '0': case '1': case '2': case '3': case '4':
+			      case '5': case '6': case '7': case '8': case '9':
+			         value = (value << 4) + aChar - '0';
+				 break;
+			  case 'a': case 'b': case 'c':
+	                      case 'd': case 'e': case 'f':
+				 value = (value << 4) + 10 + aChar - 'a';
+				 break;
+			  case 'A': case 'B': case 'C':
+	                      case 'D': case 'E': case 'F':
+				 value = (value << 4) + 10 + aChar - 'A';
+				 break;
+			  default:
+	                          throw new IllegalArgumentException(
+	                                       "Malformed \\uxxxx encoding.");
+	                    }
+	                }
+	                outBuffer.append((char)value);
+	            } else {
+	                if (aChar == 't') aChar = '\t';
+	                else if (aChar == 'r') aChar = '\r';
+	                else if (aChar == 'n') aChar = '\n';
+	                else if (aChar == 'f') aChar = '\f';
+	                outBuffer.append(aChar);
+	            }
+	        } else
+	            outBuffer.append(aChar);
+	    }
+	    return outBuffer.toString();
 	}
 
 	public File getCurrentDirectory() {
